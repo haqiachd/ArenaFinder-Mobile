04f4daf0dd10b17026be5f5eda19c0d88afa1e36
Achmad Baihaqi <haqiachd@gmail.com>
2023-09-07 14:03:30 +0700

    first update

---
+++ app/src/main/java/com/c2/arenafinder/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.c2.arenafinder.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.c2.arenafinder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
