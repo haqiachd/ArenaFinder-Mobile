4e1a0047c9871b1d4275d58cfa0319ae8253a94f
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-01 21:43:20 +0700

    update splashscreen

--- app/src/main/java/com/c2/arenafinder/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/com/c2/arenafinder/ui/activity/SplashScreenActivity.java
@@ -1,30 +1,48 @@
 package com.c2.arenafinder.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import com.airbnb.lottie.LottieAnimationView;
 
 import com.c2.arenafinder.R;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private ImageView appIcon;
+    private LottieAnimationView loading;
+
+    private void initViews(){
+        appIcon = findViewById(R.id.splash_logo_app);
+        loading = findViewById(R.id.splash_loading);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        initViews();
 
-//        new Handler().postDelayed(
-//                () -> {
-//                    startActivity(new Intent(this, AccountActivity.class));
-//                    finish();
-//                },
-//                3000L
-//        );
+        // remove status bar dan navigation bar
+        getWindow().getDecorView().post(() -> new Thread(() -> runOnUiThread(() -> {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.biru_gelap));
+        })).start());
+
+        new Handler().postDelayed(
+                () -> {
+                    startActivity(new Intent(this, AccountActivity.class));
+                    finish();
+                },
+                3000L
+        );
     }
 }
