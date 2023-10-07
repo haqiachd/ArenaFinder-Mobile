e2855e625049859da8edd1e477ccd4acedfc8c1f
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-07 21:11:43 +0700

    add empty activity for showing message & delete register activity (old)

---
+++ app/src/main/java/com/c2/arenafinder/ui/activity/EmptyActivity.java
@@ -0,0 +1,14 @@
+package com.c2.arenafinder.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
