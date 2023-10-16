7857449822568655bf9f2c4a2f838c4e74c8842c
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-16 13:09:29 +0700

    deleted deprecated fragment signupfragmentfirst

--- app/src/main/java/com/c2/arenafinder/ui/fragment/account/SignUpFirstFragment.java
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/account/SignUpFirstFragment.java
@@ -88,7 +88,7 @@ public class SignUpFirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_first2, container, false);
+        return inflater.inflate(R.layout.fragment_sign_up_first, container, false);
     }
 
     @Override
