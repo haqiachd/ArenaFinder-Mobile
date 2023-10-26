4817e4bc61b5cc316e7f42fcbb09d240cef81126
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-26 12:05:59 +0700

    update adapter and item layout

--- app/src/main/java/com/c2/arenafinder/ui/adapter/AktivitasThirdAdapter.java
+++ app/src/main/java/com/c2/arenafinder/ui/adapter/AktivitasThirdAdapter.java
@@ -3,11 +3,15 @@ package com.c2.arenafinder.ui.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.c2.arenafinder.R;
+import com.c2.arenafinder.api.retrofit.RetrofitClient;
 import com.c2.arenafinder.data.model.AktivitasFirstModel;
 import com.c2.arenafinder.data.model.AktivitasThirdModel;
 
@@ -32,7 +36,11 @@ public class AktivitasThirdAdapter extends RecyclerView.Adapter<AktivitasThirdAd
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        AktivitasThirdModel model = models.get(position);
 
+        holder.setImage(model.getImage());
+        holder.txtNamaAktivitas.setText(model.getName());
+        holder.txtVenueAktivitas.setText(model.getVenue());
     }
 
     @Override
@@ -42,11 +50,29 @@ public class AktivitasThirdAdapter extends RecyclerView.Adapter<AktivitasThirdAd
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private View view;
+        private final View view;
+
+        private final ImageView imgAktivitas;
+
+        private final TextView txtNamaAktivitas, txtVenueAktivitas;
 
         public ViewHolder(View view) {
             super(view);
             this.view = view;
+
+            this.imgAktivitas = view.findViewById(R.id.iat_image_aktivitas);
+            this.txtNamaAktivitas = view.findViewById(R.id.iat_aktivitas_name);
+            this.txtVenueAktivitas = view.findViewById(R.id.iat_aktivitas_venue);
+        }
+
+        private void setImage(String uri){
+
+            Glide.with(view)
+                    .load(RetrofitClient.AKTIVITAS_URL + uri)
+                    .placeholder(R.drawable.ic_profile)
+                    .centerCrop()
+                    .into(imgAktivitas);
+
         }
 
     }
