5446e64050613d2a2c2d3e4bb88b0990a086c136
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-28 20:46:42 +0700

    add adapter action listener interface

---
+++ app/src/main/java/com/c2/arenafinder/util/AdapterActionListener.java
@@ -0,0 +1,9 @@
+package com.c2.arenafinder.util;
+
+public interface AdapterActionListener {
+
+    default void onClickListener(int position){
+        // TODO : aksi saat item adapter di klik
+    }
+
+}
