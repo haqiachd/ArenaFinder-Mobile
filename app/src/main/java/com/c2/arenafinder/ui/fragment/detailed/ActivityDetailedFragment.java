b20134c6e461a887aab88c1133f2d1cadeeb8b1c
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-28 20:48:59 +0700

    add new fragment on detailed ActivityDetailedFragment

---
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/detailed/ActivityDetailedFragment.java
@@ -0,0 +1,56 @@
+package com.c2.arenafinder.ui.fragment.detailed;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.c2.arenafinder.R;
+
+public class ActivityDetailedFragment extends Fragment {
+
+    private static final String ID = "id";
+
+    private String id;
+
+    public ActivityDetailedFragment() {
+        // Required empty public constructor
+    }
+
+    public static ActivityDetailedFragment newInstance(String id) {
+        ActivityDetailedFragment fragment = new ActivityDetailedFragment();
+        Bundle args = new Bundle();
+        args.putString(ID, id);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            id = getArguments().getString(ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity_detailed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = view.findViewById(R.id.act_text);
+        textView.setText(id);
+    }
+}
