fb5aafb252755eb342bc13b4ab3da3cbdda54a60
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-29 23:23:51 +0700

    add new fragment BookingVenueFragment on DetailedActivity

---
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/detailed/BookingVenueFragment.java
@@ -0,0 +1,54 @@
+package com.c2.arenafinder.ui.fragment.detailed;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.c2.arenafinder.R;
+
+public class BookingVenueFragment extends Fragment {
+
+    private static final String ARG_ID = "id";
+
+    private String id;
+
+    public BookingVenueFragment() {
+        // Required empty public constructor
+    }
+
+    public static BookingVenueFragment newInstance(String id) {
+        BookingVenueFragment fragment = new BookingVenueFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_ID, id);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            id = getArguments().getString(ARG_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_booking_venue, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
