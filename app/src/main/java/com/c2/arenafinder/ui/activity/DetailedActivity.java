ab4d202122a230f42dc07259bccfc3284430b0c6
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-01 19:58:54 +0700

    show sheet dialog for choose payment

--- app/src/main/java/com/c2/arenafinder/ui/activity/DetailedActivity.java
+++ app/src/main/java/com/c2/arenafinder/ui/activity/DetailedActivity.java
@@ -2,7 +2,13 @@ package com.c2.arenafinder.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.c2.arenafinder.R;
@@ -11,6 +17,7 @@ import com.c2.arenafinder.ui.fragment.detailed.BookingVenueFragment;
 import com.c2.arenafinder.ui.fragment.detailed.VenueDetailedFragment;
 import com.c2.arenafinder.util.ArenaFinder;
 import com.c2.arenafinder.util.FragmentUtil;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.button.MaterialButton;
 
 public class DetailedActivity extends AppCompatActivity {
@@ -27,8 +34,13 @@ public class DetailedActivity extends AppCompatActivity {
 
     private MaterialButton button;
 
+    private TextView txtTop, txtData, txtRight;
+
     private void initViews(){
         button = findViewById(R.id.dtld_nav_button);
+        txtTop = findViewById(R.id.dtld_nav_txt_top);
+        txtRight = findViewById(R.id.dtld_nav_txt_right);
+        txtData = findViewById(R.id.dtld_nav_txt_data);
     }
 
     @Override
@@ -67,7 +79,20 @@ public class DetailedActivity extends AppCompatActivity {
                     break;
                 }
                 case 2 : {
-                    Toast.makeText(this, "YOU", Toast.LENGTH_SHORT).show();
+                    ArenaFinder.playVibrator(this, ArenaFinder.VIBRATOR_SHORT);
+                    BottomSheetDialog sheet = new BottomSheetDialog(this, R.style.BottomSheetTheme);
+                    View sheetInflater = getLayoutInflater().inflate(R.layout.sheet_choose_payment, null);
+                    sheet.setContentView(sheetInflater);
+
+                    sheetInflater.findViewById(R.id.scp_button).setOnClickListener(view -> {
+                        Toast.makeText(this, "Cek", Toast.LENGTH_SHORT).show();
+                    });
+
+                    sheet.show();
+                    sheet.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    sheet.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    sheet.getWindow().getAttributes().windowAnimations = R.style.BottomSheetAnim;
+                    sheet.getWindow().setGravity(Gravity.BOTTOM);
                     break;
                 }
             }
