921eaeb68b7119baef588ba3dce7d002725cdbbc
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-05 23:25:10 +0700

    add fragment view all

---
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/submain/ViewAllFragment.java
@@ -0,0 +1,49 @@
+package com.c2.arenafinder.ui.fragment.submain;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.c2.arenafinder.R;
+
+public class ViewAllFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ViewAllFragment() {
+        // Required empty public constructor
+    }
+
+    public static ViewAllFragment newInstance(String param1, String param2) {
+        ViewAllFragment fragment = new ViewAllFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_all, container, false);
+    }
+}
