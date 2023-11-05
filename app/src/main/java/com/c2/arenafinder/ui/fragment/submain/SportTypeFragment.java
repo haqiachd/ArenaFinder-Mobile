e78f5afb2273bf0e8c3eeb3621286d36b89ea65d
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-05 23:25:26 +0700

    add fragment sport type

---
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/submain/SportTypeFragment.java
@@ -0,0 +1,50 @@
+package com.c2.arenafinder.ui.fragment.submain;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.c2.arenafinder.R;
+
+public class SportTypeFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SportTypeFragment() {
+        // Required empty public constructor
+    }
+
+    public static SportTypeFragment newInstance(String param1, String param2) {
+        SportTypeFragment fragment = new SportTypeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sport_type, container, false);
+    }
+}
