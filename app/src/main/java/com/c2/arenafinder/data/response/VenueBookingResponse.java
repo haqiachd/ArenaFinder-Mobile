cc8fce370f7533d6347d018264d7d893ae7d0106
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-08 20:40:04 +0700

    add booking response

---
+++ app/src/main/java/com/c2/arenafinder/data/response/VenueBookingResponse.java
@@ -0,0 +1,51 @@
+package com.c2.arenafinder.data.response;
+
+import com.c2.arenafinder.data.model.JadwalPickerModel;
+import com.c2.arenafinder.data.model.VenueBookingModel;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class VenueBookingResponse {
+
+    @Expose
+    @SerializedName("status")
+    private String status;
+    @Expose
+    @SerializedName("message")
+    private String message;
+    @Expose
+    @SerializedName("data")
+    private VenueBookingModel data;
+
+    public VenueBookingResponse(String status, String message, VenueBookingModel data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public VenueBookingModel getData() {
+        return data;
+    }
+
+    public void setData(VenueBookingModel data) {
+        this.data = data;
+    }
+}
