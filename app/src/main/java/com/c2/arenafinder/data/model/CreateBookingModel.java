83c85fa1becf17d67f47b74e9eca8d1fb3f81840
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-08 20:38:21 +0700

    add create booking model and response

---
+++ app/src/main/java/com/c2/arenafinder/data/model/CreateBookingModel.java
@@ -0,0 +1,88 @@
+package com.c2.arenafinder.data.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CreateBookingModel {
+
+    @Expose
+    @SerializedName("id_venue")
+    private int idVenue;
+
+    @Expose
+    @SerializedName("id_booking")
+    private int idBooking;
+
+    @Expose
+    @SerializedName("id_price")
+    private int idPrice;
+
+    @Expose
+    @SerializedName("email")
+    private String email;
+
+    @Expose
+    @SerializedName("date")
+    private String date;
+
+    @Expose
+    @SerializedName("total_price")
+    private int totalPrice;
+
+    public CreateBookingModel(int idVenue, int idBooking, int idPrice, String email, String date, int totalPrice) {
+        this.idVenue = idVenue;
+        this.idBooking = idBooking;
+        this.idPrice = idPrice;
+        this.email = email;
+        this.date = date;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getIdVenue() {
+        return idVenue;
+    }
+
+    public void setIdVenue(int idVenue) {
+        this.idVenue = idVenue;
+    }
+
+    public int getIdPrice() {
+        return idPrice;
+    }
+
+    public void setIdPrice(int idPrice) {
+        this.idPrice = idPrice;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getIdBooking() {
+        return idBooking;
+    }
+
+    public void setIdBooking(int idBooking) {
+        this.idBooking = idBooking;
+    }
+}
---
+++ app/src/main/java/com/c2/arenafinder/data/response/CreateBookingResponse.java
@@ -0,0 +1,50 @@
+package com.c2.arenafinder.data.response;
+
+import com.c2.arenafinder.data.model.CreateBookingModel;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CreateBookingResponse {
+
+    @Expose
+    @SerializedName("status")
+    private String  status;
+
+    @Expose
+    @SerializedName("message")
+    private String message;
+
+    @Expose
+    @SerializedName("data")
+    private CreateBookingModel data;
+
+    public CreateBookingResponse(String status, String message, CreateBookingModel data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public CreateBookingModel getData() {
+        return data;
+    }
+
+    public void setData(CreateBookingModel data) {
+        this.data = data;
+    }
+}
