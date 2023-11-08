6097fe0ce67d5ca6b09272b9486b39d51671095e
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-08 20:44:18 +0700

    add keys on SubMainActivity

--- app/src/main/java/com/c2/arenafinder/ui/activity/SubMainActivity.java
+++ app/src/main/java/com/c2/arenafinder/ui/activity/SubMainActivity.java
@@ -3,8 +3,12 @@ package com.c2.arenafinder.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.c2.arenafinder.R;
+import com.c2.arenafinder.data.local.LogApp;
+import com.c2.arenafinder.data.local.LogTag;
 import com.c2.arenafinder.ui.fragment.submain.EditAccountFragment;
 import com.c2.arenafinder.ui.fragment.submain.MenuBookingFragment;
 import com.c2.arenafinder.ui.fragment.submain.MenuCommunityFragment;
@@ -14,19 +18,32 @@ import com.c2.arenafinder.ui.fragment.submain.SearchWorldFragment;
 import com.c2.arenafinder.ui.fragment.submain.SportTypeFragment;
 import com.c2.arenafinder.ui.fragment.submain.ViewAllFragment;
 import com.c2.arenafinder.util.FragmentUtil;
+import com.google.android.material.appbar.AppBarLayout;
 
 public class SubMainActivity extends AppCompatActivity {
 
-    public static final String FRAGMENT = "fragment";
+    public static final String FRAGMENT = "fragment", SPORT_ACTION = "sport_type", SPORT_DATA = "sport_data";
 
     public static final String EDIT_ACCOUNT = "edit_acc", MENU_BOOKING = "booking", MENU_COMMUNITY = "community",
             MENU_TROLLEY = "trolley", NOTIFICATIONS = "notif", SEARCH_WORLD = "search",
             SPORT_TYPE = "sport", VIEW_ALL = "view";
 
+    private AppBarLayout appBarLayout;
+    private ImageView btnBack;
+    private TextView appbarTitle;
+
+    private void initViews() {
+        appBarLayout = findViewById(R.id.sub_appbar);
+        btnBack = findViewById(R.id.sub_back);
+        appbarTitle = findViewById(R.id.sub_title);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_main);
+
+        initViews();
 
         switch (getIntent().getStringExtra(FRAGMENT)) {
             case EDIT_ACCOUNT: {
@@ -66,9 +83,21 @@ public class SubMainActivity extends AppCompatActivity {
                 break;
             }
             case SPORT_TYPE: {
-                FragmentUtil.switchFragmentSubMain(
-                        getSupportFragmentManager(), new SportTypeFragment(), false
-                );
+                String action = getIntent().getStringExtra(SPORT_ACTION),
+                        data = getIntent().getStringExtra(SPORT_DATA);
+                LogApp.info(this, LogTag.LIFEFCYLE, "ACTION --> " + action);
+                LogApp.info(this, LogTag.LIFEFCYLE, "DATA   --> " + data);
+                if (action != null && data != null){
+                    FragmentUtil.switchFragmentSubMain(
+                            getSupportFragmentManager(),
+                            SportTypeFragment.newInstance(Integer.parseInt(action), data),
+                            false
+                    );
+                }else {
+                    FragmentUtil.switchFragmentSubMain(
+                            getSupportFragmentManager(), new SportTypeFragment(), false
+                    );
+                }
                 break;
             }
             case VIEW_ALL: {
@@ -78,5 +107,16 @@ public class SubMainActivity extends AppCompatActivity {
                 break;
             }
         }
+
+        onClickGroups();
     }
+
+    private void onClickGroups() {
+
+        btnBack.setOnClickListener(v -> {
+            super.onBackPressed();
+        });
+
+    }
+
 }
