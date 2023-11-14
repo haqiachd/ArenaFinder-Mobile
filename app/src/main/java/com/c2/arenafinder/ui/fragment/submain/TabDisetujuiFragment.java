38917e4ecebe94d14bf0c05e45a6ccce5cb0b8c5
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-14 19:52:24 +0700

    update tab disetujui and tab di tolak

--- app/src/main/java/com/c2/arenafinder/ui/fragment/submain/TabDisetujuiFragment.java
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/submain/TabDisetujuiFragment.java
@@ -2,43 +2,49 @@ package com.c2.arenafinder.ui.fragment.submain;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.c2.arenafinder.R;
+import com.c2.arenafinder.api.retrofit.RetrofitClient;
+import com.c2.arenafinder.data.model.StatusPesananModel;
+import com.c2.arenafinder.data.response.StatusPesananResponse;
+import com.c2.arenafinder.ui.adapter.StatusPesananAdapter;
+import com.c2.arenafinder.util.UsersUtil;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TabDisetujuiFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TabDisetujuiFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private UsersUtil usersUtil;
+
+    private RecyclerView recyclerView;
 
     public TabDisetujuiFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TabDisetujuiFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public void initViews(View root) {
+        recyclerView = root.findViewById(R.id.tab_disetujui_recycler);
+    }
+
     public static TabDisetujuiFragment newInstance(String param1, String param2) {
         TabDisetujuiFragment fragment = new TabDisetujuiFragment();
         Bundle args = new Bundle();
@@ -63,4 +69,46 @@ public class TabDisetujuiFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab_disetujui, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+        usersUtil = new UsersUtil(requireContext());
+
+        fetchData();
+    }
+
+    private void fetchData() {
+
+        RetrofitClient.getInstance().getStatusPesanan(usersUtil.getEmail(), StatusPesananModel.DI_SETUJUI)
+                .enqueue(new Callback<StatusPesananResponse>() {
+                    @Override
+                    public void onResponse(Call<StatusPesananResponse> call, Response<StatusPesananResponse> response) {
+                        if (response.body() != null && response.body().getStatus().equalsIgnoreCase(RetrofitClient.SUCCESSFUL_RESPONSE)) {
+                            showData(response.body().getData());
+                        } else {
+                            Toast.makeText(requireContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StatusPesananResponse> call, Throwable t) {
+                        Toast.makeText(requireContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showData(ArrayList<StatusPesananModel> models){
+        if(models.size() <= 0){
+            recyclerView.setVisibility(View.GONE);
+        }else {
+            if (isAdded()){
+                recyclerView.setAdapter(new StatusPesananAdapter(
+                        requireContext(), models
+                ));
+            }
+        }
+    }
+
 }
