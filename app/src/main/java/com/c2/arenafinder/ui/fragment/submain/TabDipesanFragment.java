7c030f4f57d0b717c55b89c7477dfd3af92a931b
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-14 19:51:30 +0700

    update tab di pesan

--- app/src/main/java/com/c2/arenafinder/ui/fragment/submain/TabDipesanFragment.java
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/submain/TabDipesanFragment.java
@@ -10,42 +10,41 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.c2.arenafinder.R;
+import com.c2.arenafinder.api.retrofit.RetrofitClient;
 import com.c2.arenafinder.data.model.StatusPesananModel;
+import com.c2.arenafinder.data.response.StatusPesananResponse;
 import com.c2.arenafinder.ui.adapter.StatusPesananAdapter;
+import com.c2.arenafinder.util.UsersUtil;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TabDipesanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TabDipesanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private UsersUtil usersUtil;
+
+    private RecyclerView recyclerView;
 
     public TabDipesanFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TabDipesanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public void initViews(View root) {
+        recyclerView = root.findViewById(R.id.tab_dipesan_recycler);
+    }
+
     public static TabDipesanFragment newInstance(String param1, String param2) {
         TabDipesanFragment fragment = new TabDipesanFragment();
         Bundle args = new Bundle();
@@ -74,18 +73,42 @@ public class TabDipesanFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+        usersUtil = new UsersUtil(requireContext());
 
-        RecyclerView recyclerView = view.findViewById(R.id.ftd_recycler);
-
-        ArrayList<StatusPesananModel> models = new ArrayList<>();
-        models.add(new StatusPesananModel(1));
-        models.add(new StatusPesananModel(1));
-        models.add(new StatusPesananModel(1));
-        models.add(new StatusPesananModel(1));
-
-        recyclerView.setAdapter(new StatusPesananAdapter(
-                models
-        ));
-
+        fetchData();
     }
+
+    private void fetchData() {
+
+        RetrofitClient.getInstance().getStatusPesanan(usersUtil.getEmail(), StatusPesananModel.DI_PESAN)
+                .enqueue(new Callback<StatusPesananResponse>() {
+                    @Override
+                    public void onResponse(Call<StatusPesananResponse> call, Response<StatusPesananResponse> response) {
+                        if (response.body() != null && response.body().getStatus().equalsIgnoreCase(RetrofitClient.SUCCESSFUL_RESPONSE)) {
+                            showData(response.body().getData());
+                        } else {
+                            Toast.makeText(requireContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StatusPesananResponse> call, Throwable t) {
+                        Toast.makeText(requireContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showData(ArrayList<StatusPesananModel> models){
+        if(models.size() <= 0){
+            recyclerView.setVisibility(View.GONE);
+        }else {
+            if (isAdded()){
+                recyclerView.setAdapter(new StatusPesananAdapter(
+                        requireContext(), models
+                ));
+            }
+        }
+    }
+
 }
