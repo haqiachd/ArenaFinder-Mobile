7e752101c7e2d0cf614d019401dd97cd87cd5912
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-21 19:51:16 +0700

    migrate fragment util to kotlin language

--- app/src/main/java/com/c2/arenafinder/util/FragmentUtil.kt
+++ app/src/main/java/com/c2/arenafinder/util/FragmentUtil.kt
@@ -1,62 +1,100 @@
-package com.c2.arenafinder.util;
+package com.c2.arenafinder.util
 
-import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.annotation.AnimRes
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
-import com.c2.arenafinder.R;
-import com.c2.arenafinder.data.local.LogApp;
-import com.c2.arenafinder.data.local.LogTag;
+import com.c2.arenafinder.R
+import com.c2.arenafinder.data.local.LogApp
+import com.c2.arenafinder.data.local.LogTag
 
-public class FragmentUtil {
+object FragmentUtil {
 
-    private static void switchFragment(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, @IdRes int frameLayout, boolean backStack
-    ){
-        if (fragment != null){
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            if (backStack){
-                transaction.replace(frameLayout, fragment)
-                        .addToBackStack(null)
-                        .commit();
-            }else {
-                transaction.replace(frameLayout, fragment).commit();
+    private const val defaultAnim = R.anim.anim_default_anim
+
+    private fun switchFragment(
+        manager: FragmentManager, fragment: Fragment?,
+        @IdRes frameLayout: Int,
+        backStack: Boolean,
+        @AnimRes enter: Int,
+        @AnimRes exit: Int,
+        @AnimRes popEnter: Int,
+        @AnimRes popExit: Int,
+    ) {
+        if (fragment != null) {
+            // create fragment transaction
+            val fragmentTransaction = manager.beginTransaction()
+            fragmentTransaction.setCustomAnimations(enter, exit, popEnter, popExit)
+            // backstack action
+            if (backStack) {
+                fragmentTransaction.replace(frameLayout, fragment)
+                fragmentTransaction.addToBackStack(null).commit()
+            } else {
+                fragmentTransaction.replace(frameLayout, fragment).commit()
             }
-        }else {
-            LogApp.error("OBJECT", LogTag.LIFEFCYLE,"ERROR FRAGMENT NO ATTACHED ON ACTIVITY");
+        } else {
+            LogApp.error("OBJECT", LogTag.LIFEFCYLE, "ERROR FRAGMENT NO ATTACHED ON ACTIVITY")
         }
     }
 
-    public static void switchFragmentAccount(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, boolean backStack
-    ){
-        switchFragment(fragmentManager, fragment, R.id.acc_frame_layout, backStack);
+    @JvmOverloads
+    @JvmStatic
+    fun switchFragmentAccount(
+        fragmentManager: FragmentManager, fragment: Fragment?, backStack: Boolean,
+        @AnimRes enter: Int = defaultAnim,
+        @AnimRes exit: Int = defaultAnim,
+        @AnimRes popEnter: Int = defaultAnim,
+        @AnimRes popExit: Int = defaultAnim,
+    ) {
+        switchFragment(fragmentManager, fragment, R.id.acc_frame_layout, backStack, enter, exit, popEnter, popExit)
     }
 
-    public static void switchFragmentMain(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, boolean backStack
-    ){
-        switchFragment(fragmentManager, fragment, R.id.main_frame, backStack);
+    @JvmOverloads
+    @JvmStatic
+    fun switchFragmentMain(
+        fragmentManager: FragmentManager, fragment: Fragment?, backStack: Boolean,
+        @AnimRes enter: Int = defaultAnim,
+        @AnimRes exit: Int = defaultAnim,
+        @AnimRes popEnter: Int = defaultAnim,
+        @AnimRes popExit: Int = defaultAnim,
+    ) {
+        switchFragment(fragmentManager, fragment, R.id.main_frame, backStack, enter, exit, popEnter, popExit)
     }
 
-    public static void switchFragmentSubMain(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, boolean backStack
-    ){
-        switchFragment(fragmentManager, fragment, R.id.sub_frame_layout, backStack);
+    @JvmOverloads
+    @JvmStatic
+    fun switchFragmentSubMain(
+        fragmentManager: FragmentManager, fragment: Fragment?, backStack: Boolean,
+        @AnimRes enter: Int = defaultAnim,
+        @AnimRes exit: Int = defaultAnim,
+        @AnimRes popEnter: Int = defaultAnim,
+        @AnimRes popExit: Int = defaultAnim,
+    ) {
+        switchFragment(fragmentManager, fragment, R.id.sub_frame_layout, backStack, enter, exit, popEnter, popExit)
     }
 
-    public static void switchFragmentEmpty(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, boolean backStack
-    ){
-        switchFragment(fragmentManager, fragment, R.id.empty_frame_layout, backStack);
+    @JvmOverloads
+    @JvmStatic
+    fun switchFragmentEmpty(
+        fragmentManager: FragmentManager, fragment: Fragment?, backStack: Boolean,
+        @AnimRes enter: Int = defaultAnim,
+        @AnimRes exit: Int = defaultAnim,
+        @AnimRes popEnter: Int = defaultAnim,
+        @AnimRes popExit: Int = defaultAnim,
+    ) {
+        switchFragment(fragmentManager, fragment, R.id.empty_frame_layout, backStack, enter, exit, popEnter, popExit)
     }
 
-    public static void switchFragmentDetailed(
-            @NonNull FragmentManager fragmentManager, Fragment fragment, boolean backStack
-    ){
-        switchFragment(fragmentManager, fragment, R.id.detailed_frame_layout, backStack);
+    @JvmOverloads
+    @JvmStatic
+    fun switchFragmentDetailed(
+        fragmentManager: FragmentManager, fragment: Fragment?, backStack: Boolean,
+        @AnimRes enter: Int = defaultAnim,
+        @AnimRes exit: Int = defaultAnim,
+        @AnimRes popEnter: Int = defaultAnim,
+        @AnimRes popExit: Int = defaultAnim,
+    ) {
+        switchFragment(fragmentManager, fragment, R.id.detailed_frame_layout, backStack, enter, exit, popEnter, popExit)
     }
-
 }
