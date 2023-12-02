4bf778c95bbe3c7e7720c34e147125734960fb72
Achmad Baihaqi <haqiachd@gmail.com>
2023-12-02 20:42:41 +0700

    delete unused files

--- app/src/main/java/com/c2/arenafinder/di/ArenaFinderViewModelFactory.java
+++
@@ -1,5 +0,0 @@
-package com.c2.arenafinder.di;
-
-public class ArenaFinderViewModelFactory {
-    // TODO : This is a factory class for inputting constructors into the ViewModel.
-}
--- app/src/main/java/com/c2/arenafinder/viewmodel/ArenaFinderViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.c2.arenafinder.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class ArenaFinderViewModel extends ViewModel {
-    // TODO :
-    //  This is a ViewModel class used to bridge between the UI display and the data model (controllers).
-}
