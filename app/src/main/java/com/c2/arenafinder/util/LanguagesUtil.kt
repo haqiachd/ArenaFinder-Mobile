389a549c1c4757a0a9754263b8c73b7ff49e79b5
Achmad Baihaqi <haqiachd@gmail.com>
2023-12-02 20:53:01 +0700

    update language util

--- app/src/main/java/com/c2/arenafinder/util/LanguagesUtil.kt
+++ app/src/main/java/com/c2/arenafinder/util/LanguagesUtil.kt
@@ -2,23 +2,24 @@ package com.c2.arenafinder.util
 
 import android.app.Activity
 import android.os.Build
+import java.util.Locale
+
 import com.c2.arenafinder.data.local.DataShared
 import com.c2.arenafinder.data.model.LanguageModel
-import java.util.*
 
 class LanguagesUtil(
     private val activity: Activity,
     private val dataShared: DataShared,
 ) {
 
-    companion object{
+    companion object {
         const val ENGLISH = "en"
         const val INDONESIAN = "id" // def
         const val JAVANESE = "jv"
     }
 
     @JvmOverloads
-    fun changeLanguage(langCode : String = getActivatedLanguage()){
+    fun changeLanguage(langCode: String = getActivatedLanguage()) {
         val desiredLocale = Locale(langCode)
         val config = activity.resources.configuration
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -31,14 +32,21 @@ class LanguagesUtil(
         activity.resources.updateConfiguration(config, activity.resources.displayMetrics)
     }
 
-    fun getActivatedLanguage() : String = dataShared.getData(DataShared.KEY.APP_LANGUAGE)
+    fun getActivatedLanguage(): String {
+        return try {
+            dataShared.getData(DataShared.KEY.APP_LANGUAGE) ?: INDONESIAN
+        } catch (ex: Throwable) {
+            ex.printStackTrace()
+            INDONESIAN
+        }
+    }
 
-    fun setActivatedLanguage(langCode: String){
+    fun setActivatedLanguage(langCode: String) {
         dataShared.setData(DataShared.KEY.APP_LANGUAGE, langCode)
     }
 
-    fun getLanguageModel(languageModel: LanguageModel) : String{
-        return when(getActivatedLanguage()){
+    fun getLanguageModel(languageModel: LanguageModel): String {
+        return when (getActivatedLanguage()) {
             ENGLISH -> languageModel.english
             INDONESIAN -> languageModel.indonesian
             JAVANESE -> languageModel.javanese
