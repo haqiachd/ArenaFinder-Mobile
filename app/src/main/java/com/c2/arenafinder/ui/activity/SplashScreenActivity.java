3804b74ca4c935519adab799503666b3aec7567d
Achmad Baihaqi <haqiachd@gmail.com>
2023-12-02 20:48:47 +0700

    handler when server is off

--- app/src/main/java/com/c2/arenafinder/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/com/c2/arenafinder/ui/activity/SplashScreenActivity.java
@@ -1,18 +1,22 @@
 package com.c2.arenafinder.ui.activity;
 
 import static com.c2.arenafinder.data.local.DataShared.KEY.IS_FIRST_TIME_INSTALL;
-import static com.c2.arenafinder.data.local.DataShared.KEY.APP_LANGUAGE;
+import static com.c2.arenafinder.data.local.DataShared.KEY.SAVED_DEVICE_TOKEN;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -21,19 +25,22 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 
 import com.c2.arenafinder.R;
-import com.c2.arenafinder.api.retrofit.RetrofitClient;
+import com.c2.arenafinder.api.retrofit.RetrofitState;
 import com.c2.arenafinder.data.local.DataShared;
 import com.c2.arenafinder.data.local.LogApp;
 import com.c2.arenafinder.data.local.LogTag;
+import com.c2.arenafinder.data.model.ArenaFinderModel;
+import com.c2.arenafinder.data.repository.ArenaFinderRepository;
 import com.c2.arenafinder.data.response.ArenaFinderResponse;
+import com.c2.arenafinder.di.ArenaFinderViewModelFactory;
 import com.c2.arenafinder.util.ArenaFinder;
 import com.c2.arenafinder.util.LanguagesUtil;
 import com.c2.arenafinder.util.UsersUtil;
+import com.c2.arenafinder.viewmodel.ArenaFinderViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
@@ -43,18 +50,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     private final String[] allPermission = {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_PHONE_STATE
     };
 
     private UsersUtil usersUtil;
 
     private DataShared shared;
 
-    private LanguagesUtil languagesUtil;
-
     private LottieAnimationView loading;
 
-    private void initViews(){
+    private ArenaFinderViewModel viewModel;
+
+    private void initViews() {
         loading = findViewById(R.id.splash_loading);
     }
 
@@ -71,77 +79,164 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         initViews();
 
-        // initialize firebaes
+        // create view model
+        viewModel = new ViewModelProvider(
+                this,
+                new ArenaFinderViewModelFactory(new ArenaFinderRepository())
+        ).get(ArenaFinderViewModel.class);
+
+        // initialize firebase
         FirebaseApp.initializeApp(this.getApplicationContext());
 
         // initialize class
         usersUtil = new UsersUtil(this);
         shared = new DataShared(this);
-        languagesUtil = new LanguagesUtil(this, shared);
+        LanguagesUtil languagesUtil = new LanguagesUtil(this, shared);
 
-        if (!shared.contains(IS_FIRST_TIME_INSTALL)){
-            shared.setData(IS_FIRST_TIME_INSTALL, "NO");
+        // handling app status
+        try {
+            // first time installed
+            if (!shared.contains(IS_FIRST_TIME_INSTALL)) {
+                LogApp.error(this, LogTag.LIFEFCYLE, "FIRST TIME INSTALL");
+                shared.resetDefaultValue();
+                languagesUtil.setActivatedLanguage(LanguagesUtil.INDONESIAN);
+                languagesUtil.changeLanguage(LanguagesUtil.INDONESIAN);
+                callObserver();
+            } else {
+                LogApp.error(this, LogTag.LIFEFCYLE, "HAS BEEN INSTALLED");
+                callObserver();
+            }
+        } catch (Throwable ex) {
+            // when error status
+            ex.printStackTrace();
+            shared.resetDefaultValue();
+            LogApp.error(this, LogTag.LIFEFCYLE, "FIRST TIME INSTALL WITH ERROR");
             languagesUtil.setActivatedLanguage(LanguagesUtil.INDONESIAN);
+            languagesUtil.changeLanguage(LanguagesUtil.INDONESIAN);
+            callObserver();
         }
 
-        languagesUtil.changeLanguage();
-
-        // cek koneksi
-        new Handler().postDelayed(this::cekKoneksi, 900L);
+        observer();
     }
 
-    private void cekKoneksi(){
+    private void callObserver() {
+        new Handler().postDelayed(() -> viewModel.doCheckKoneksi(), 1000L);
+    }
 
-        RetrofitClient.getInstance().cekKoneksi().enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<ArenaFinderResponse> call, Response<ArenaFinderResponse> response) {
+    private void observer() {
 
-                if (response.body() != null && response.body().getStatus().equalsIgnoreCase("success")) {
+        viewModel.cekKoneksi().observe(this, dataState -> {
+            if (dataState instanceof RetrofitState.Loading) {
+                LogApp.info(this, LogTag.RETROFIT_ON_LOADING, "On SplashScreen Loading");
+            } else if (dataState instanceof RetrofitState.Error) {
+                LogApp.info(this, LogTag.RETROFIT_ON_LOADING, "On SplashScreen Failure");
 
-                    // jika aplikasi pertama kali di install maka akan menampilkan request permission
-                    if (!shared.contains(IS_FIRST_TIME_INSTALL)){
+                // get error message and handler the problem
+                String message = ((RetrofitState.Error) dataState).getMessage();
+
+                // handler server not found
+                if (message.contains("server not found")) {
+                    // membuka fragment server not found
+                    ArenaFinder.VibratorToast(this, R.string.err_server_not_found, Toast.LENGTH_LONG, ArenaFinder.VIBRATOR_LONG);
+                    startActivity(new Intent(SplashScreenActivity.this, EmptyActivity.class)
+                            .putExtra(EmptyActivity.FRAGMENT, EmptyActivity.SERVER_NOT_FOUND)
+                    );
+                    finish();
+                }
+                // handler timeout
+                else if (message.contains("timeout")) {
+                    ArenaFinder.playVibrator(this, ArenaFinder.VIBRATOR_SHORT);
+                    loading.cancelAnimation();
+                    requestNextPermission();
+                }
+                // unexpected error
+                else {
+                    ArenaFinder.VibratorToast(this, message, Toast.LENGTH_LONG, ArenaFinder.VIBRATOR_LONG);
+                }
+            } else if (dataState instanceof RetrofitState.Success) {
+                LogApp.info(this, LogTag.RETROFIT_ON_LOADING, "On SplashScreen Response");
+                ArenaFinderModel model = ((RetrofitState.Success<ArenaFinderResponse>) dataState).getData().getData();
+
+                LogApp.info(this, LogTag.LIFEFCYLE, "Server Status : " + model.getServerStatus());
+                LogApp.info(this, LogTag.LIFEFCYLE, "Have Update : " + model.getHaveUpdate());
+                LogApp.info(this, LogTag.LIFEFCYLE, "Min Version Code : " + model.getMinVersionCode());
+                LogApp.info(this, LogTag.LIFEFCYLE, "New Version Name : " + model.getNewVersionName());
+                LogApp.info(this, LogTag.LIFEFCYLE, "Update Link : " + model.getUpdateLink());
+                LogApp.info(this, LogTag.LIFEFCYLE, "Desc Update : " + model.getDescUpdate());
+
+                // check status server
+                if (model.getServerStatus()) {
+                    // handler update app
+                    try {
+                        // get version code
+                        PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+                        int versionCode = packageInfo.versionCode;
+
+                        // check update
+                        if (model.getHaveUpdate()) {
+                            // check whether the user needs to update
+                            if (model.getMinVersionCode() > versionCode) {
+                                loading.cancelAnimation();
+                                new AlertDialog.Builder(this)
+                                        .setTitle(R.string.dia_title_update)
+                                        .setMessage(getString(R.string.dia_msg_update, model.getDescUpdate()))
+                                        .setPositiveButton(R.string.btn_dia_update, (dialog, which) -> {
+                                            LogApp.info(this, LogTag.LIFEFCYLE, "ACTION 1");
+                                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(model.getUpdateLink())));
+                                            // open app
+                                            requestNextPermission();
+                                        })
+                                        .setNegativeButton(R.string.btn_dia_skip, (dialog, which) -> {
+                                            // open app
+                                            LogApp.info(this, LogTag.LIFEFCYLE, "ACTION 2");
+                                            requestNextPermission();
+                                        })
+                                        .setCancelable(false)
+                                        .create().show();
+                            }
+                        } else {
+                            // open app
+                            LogApp.info(this, LogTag.LIFEFCYLE, "ACTION 1");
+                            requestNextPermission();
+                        }
+
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        // skip update
                         loading.cancelAnimation();
                         requestNextPermission();
-                    }else {
-                        createUniverse();
                     }
-                } else {
-                    ArenaFinder.playVibrator(getApplicationContext(), ArenaFinder.VIBRATOR_LONG);
-//                    startActivity(new Intent(SplashScreenActivity.this, EmptyActivity.class)
-//                            .putExtra(EmptyActivity.FRAGMENT, EmptyActivity.SERVER_NOT_FOUND)
-//                    );
-//                    finish();
 
-                    // temp
-                    Toast.makeText(SplashScreenActivity.this, "Server tidak ditemukan!", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                } else {
+                    // handler when server not active
+                    startActivity(
+                            new Intent(this, EmptyActivity.class)
+                                    .putExtra(EmptyActivity.FRAGMENT, EmptyActivity.SERVER_OFF)
+                    );
                 }
             }
-
-            @Override
-            public void onFailure(Call<ArenaFinderResponse> call, Throwable t) {
-                t.printStackTrace();
-                ArenaFinder.playVibrator(getApplicationContext(), ArenaFinder.VIBRATOR_LONG);
-//                startActivity(new Intent(SplashScreenActivity.this, EmptyActivity.class)
-//                        .putExtra(EmptyActivity.FRAGMENT, EmptyActivity.SERVER_NOT_FOUND)
-//                );
-//                finish();
-
-                // temp
-                Toast.makeText(SplashScreenActivity.this, "Server tidak ditemukan!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-            }
         });
-
     }
 
-    private void createUniverse(){
+    private void createUniverse() {
         if (usersUtil.isSignIn()) {
-            // Jika pengguna sudah masuk, buka MainActivity
-            LogApp.info(this, LogTag.LIFEFCYLE, "Membuka MainActivity");
-//            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<>() {
+                @Override
+                public void onComplete(@NonNull Task<String> task) {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        String token = task.getResult();
 
+                        if (!shared.getData(SAVED_DEVICE_TOKEN).equals(token)){
+
+                        }
+                    } else {
+                        // Jika pengguna sudah masuk, buka MainActivity
+                        LogApp.info(this, LogTag.LIFEFCYLE, "Membuka MainActivity");
+                        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    }
+                }
+            });
         } else {
             // Jika pengguna belum masuk, buka WelcomeActivity
             LogApp.info(this, LogTag.LIFEFCYLE, "Membuka WelcomeActivity");
@@ -155,6 +250,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     private void requestNextPermission() {
         if (currentPermissionIndex < allPermission.length) {
+            LogApp.info(this, LogTag.LIFEFCYLE, "permission");
             String permission = allPermission[currentPermissionIndex];
             if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
                 currentPermissionIndex++;
@@ -163,6 +259,7 @@ public class SplashScreenActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{permission}, getPermissionRequestCode());
             }
         } else {
+            LogApp.info(this, LogTag.LIFEFCYLE, "create");
             createUniverse();
         }
     }
@@ -175,6 +272,8 @@ public class SplashScreenActivity extends AppCompatActivity {
                 return ArenaFinder.PERMISSION_STORAGE;
             case Manifest.permission.CAMERA:
                 return ArenaFinder.PERMISSION_CAMERA;
+            case Manifest.permission.READ_PHONE_STATE:
+                return ArenaFinder.PERMISSION_PHONE;
             default:
                 return -1;
         }
---
+++ app/src/main/java/com/c2/arenafinder/ui/fragment/empty/ServerOffFragment.java
@@ -0,0 +1,111 @@
+package com.c2.arenafinder.ui.fragment.empty;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.c2.arenafinder.R;
+import com.c2.arenafinder.api.retrofit.RetrofitClient;
+import com.c2.arenafinder.data.response.ArenaFinderResponse;
+import com.c2.arenafinder.ui.activity.SplashScreenActivity;
+import com.c2.arenafinder.util.ArenaFinder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ServerOffFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private SwipeRefreshLayout refreshLayout;
+
+    private void initViews(View root){
+        refreshLayout = root.findViewById(R.id.fso_swipe);
+    }
+
+    public ServerOffFragment() {
+        // Required empty public constructor
+    }
+
+    public static ServerOffFragment newInstance(String param1, String param2) {
+        ServerOffFragment fragment = new ServerOffFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_server_off, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshAction();
+                        refreshLayout.setRefreshing(false);
+                    }
+                }, ArenaFinder.MILLIS_OF_REFRESHING);
+            }
+        });
+    }
+
+    private void refreshAction(){
+
+        RetrofitClient.getInstance().cekKoneksi().enqueue(new Callback<ArenaFinderResponse>() {
+            @Override
+            public void onResponse(@Nullable Call<ArenaFinderResponse> call, @Nullable Response<ArenaFinderResponse> response) {
+                assert response != null;
+                if(response.body() != null && response.body().getStatus().equalsIgnoreCase("success")){
+                    ArenaFinder.restartApplication(requireContext(), SplashScreenActivity.class);
+                }else {
+                    ArenaFinder.playVibrator(requireContext(), ArenaFinder.VIBRATOR_SHORT);
+                    Toast.makeText(requireContext(), R.string.err_server_not_found, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@Nullable  Call<ArenaFinderResponse> call, @Nullable  Throwable t) {
+                ArenaFinder.playVibrator(requireContext(), ArenaFinder.VIBRATOR_SHORT);
+                Toast.makeText(requireContext(), R.string.err_server_not_found, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
